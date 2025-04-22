59ba7424aaedb9a31821f5a2e940f3280320f68c
sangdo14 <sangdo14@naver.com>
2025-04-22 15:47:10 +0900

    페이지 처리 링크

--- src/main/java/com/example/basic/controller/JoinController.java
+++ src/main/java/com/example/basic/controller/JoinController.java
@@ -3,43 +3,36 @@ import com.example.basic.dto.JoinDTO;
 import com.example.basic.entity.JoinEntity;
 import com.example.basic.service.JoinService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-//Model: 컨트롤러에서 템플릿에 데이터를 전달시 필요한 스프링 내장 객체
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-// 인스턴스를 담을 멤버변수에 final 지정 가능케 함
 @RequiredArgsConstructor
 public class JoinController {
-    // 서비스 계층으로부터 joinService라는 인스턴스 @Autowired라는 어노테이션을 통해 바로 해당 인스턴스 객체를 바로 가져올 수 있음 (new 연산자 호출 필요없음)
-    // @Autowired
     private final JoinService joinService;
 
-    // 처음 조인을 화면 출력하는 get방식 유형
     @GetMapping("/join")
-    public String join() {
+    public String join(){
         return "join";
     }
 
-    // 해당 조인폼 화면에서 폼의 전송 이벤트 발생시 데이터 가공하는 post 방식 요청
-    // 이때 폼의 모든 요소를 일일이 전달하는 것이 아닌 DTO파일로 감싸서 전달
-    // Model타입의 파라미터, 서비스에 전달된 데이터를 뷰템플릿에 전달하기 위한 전용 클래스
     @PostMapping("/join/create")
-    public String create( JoinEntity formDTO ) {
+    public String create( JoinDTO formDTO ){
         joinService.processJoin(formDTO);
-//        String msg = joinService.processJoin(formDTO);
-//        model.addAttribute("data", msg);
         return "redirect:/admin";
     }
 
     @GetMapping("/admin")
-    public String showAdminPage(Model model){
-        List<JoinEntity> users = joinService.getAllUsers();
-        model.addAttribute("users", users);
+    public String showAdminPage(@RequestParam(defaultValue="0") int page, Model model){
+        //각 페이지별 출력할 데이터 개수
+        int pageSize = 3;
+        Page<JoinEntity> userPage = joinService.getUsersByPage(page, pageSize);
+        model.addAttribute("userPage", userPage);
+        model.addAttribute("currentPage", page);
         return "admin";
     }
 
@@ -56,9 +49,14 @@ public class JoinController {
         return "edit";
     }
 
+
     @PostMapping("/admin/update")
-    public String updateUser(JoinEntity formuser){
-        joinService.updateUser(formuser);
+    public String updateUser(JoinEntity formUser){
+        joinService.updateUser(formUser);
         return "redirect:/admin";
     }
+
+
+
+
 }
--- src/main/java/com/example/basic/service/JoinService.java
+++ src/main/java/com/example/basic/service/JoinService.java
@@ -1,8 +1,11 @@
 package com.example.basic.service;
+
 import com.example.basic.dto.JoinDTO;
 import com.example.basic.entity.JoinEntity;
 import com.example.basic.repository.JoinRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,37 +15,30 @@ import java.util.List;
 public class JoinService {
     private final JoinRepo joinRepo;
 
-    public void processJoin(JoinEntity dto) {
-        // Entity클래스로 부터 각 항목에 맞는 데이터를 인자로 전달하여 실제 db에 저장할 모델 인스턴스생성
-        JoinEntity user = new JoinEntity(null, dto.getUname(), dto.getEmail(), dto.getColors());
-
-        //위에서 생성한 user라는 인스턴스를 joinRepo의 save메서드로 전달하기만 하면 DB에 데이터 저장됨
-        //save() 리포지토리에서 JPA구현체가 자동으로 생성 DB저장 전용 메서드
+    public void processJoin(JoinDTO dto) {
+        JoinEntity user = new JoinEntity(null, dto.getUname(), dto.getEmail(), dto.getColors() );
         joinRepo.save(user);
-
-        // 컨트롤러를 통해 실제 템플릿에 전달된 데이터가 매핑된 태그 문자열 반환
-//        String result = "</br><ul><li>name: " + dto.getUname() + "</li>"
-//                + "<li>email: " + dto.getEmail() + "</li>"
-//                + "<li>my color: " + dto.getColors() + "</li></ul></br>";
-//        return result;
     }
 
-    //모든 db데이터 조회해서 반환
-    public List<JoinEntity> getAllUsers(){ return joinRepo.findAll();}
+    public List<JoinEntity> getAllUsers(){
+        return joinRepo.findAll();
+    }
 
-    //데이터 삭제하는 jpo메서드 호출
-    public void delete(Long id){ joinRepo.deleteById(id); }
+    public void delete(Long id){
+        joinRepo.deleteById(id);
+    }
 
-    //id값으로 해당 데이터만 가져오는 메서드
     public JoinEntity getUserById(Long id){
-        //초기 에러를 피하기 위해서 onElseThrow룰 통해 예외처리
-        return joinRepo.findById(id).orElseThrow(()->new RuntimeException("해당 아이디의 유저 없음"));
+        return joinRepo.findById(id).orElseThrow(()-> new RuntimeException("해당 아이디의 유저 없음"));
     }
 
     public void updateUser(JoinEntity user){
         joinRepo.save(user);
     }
+
+    //페이지 번호에 따라 유저 데이터 가져오는 메서드
+    public Page<JoinEntity> getUsersByPage(int page, int size){
+        //page: 페이지 번호,  size: 한페이지에 불어올 데이터 갯수
+        return joinRepo.findAll(PageRequest.of(page, size));
+    }
 }
-/*
-    JPA Respository의 dirty che
- */
